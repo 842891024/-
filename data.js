2bf8146acfa293b70c46ef9fc4dad08b5da66245
Loye <842891024@qq.com>
2015-08-26 09:28:42 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+var data = [
+	{"index":"1","img":"photo/1.jpg","cpname":"test","author":"testauthor"},
+	{"index":"2","img":"photo/2.jpg","cpname":"test","author":"testauthor"},
+	{"index":"3","img":"photo/3.jpg","cpname":"test","author":"testauthor"},
+	{"index":"4","img":"photo/4.jpg","cpname":"test","author":"testauthor"},
+	{"index":"5","img":"photo/5.jpg","cpname":"test","author":"testauthor"},
+	{"index":"6","img":"photo/6.jpg","cpname":"test","author":"testauthor"},
+	{"index":"7","img":"photo/7.jpg","cpname":"test","author":"testauthor"},
+	{"index":"8","img":"photo/8.jpg","cpname":"test","author":"testauthor"},
+	{"index":"9","img":"photo/9.jpg","cpname":"test","author":"testauthor"},
+	{"index":"10","img":"photo/10.jpg","cpname":"test","author":"testauthor"},
+	{"index":"11","img":"photo/11.jpg","cpname":"test","author":"testauthor"},
+	{"index":"12","img":"photo/12.jpg","cpname":"test","author":"testauthor"},
+]
---
+++ photo/data.js
@@ -0,0 +1,14 @@
+var data = [
+	{"img":"photo/1.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/2.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/3.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/4.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/5.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/6.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/7.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/8.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/9.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/10.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/11.jpg","cpname":"test","author":"testauthor"},
+	{"img":"photo/12.jpg","cpname":"test","author":"testauthor"},
+]
